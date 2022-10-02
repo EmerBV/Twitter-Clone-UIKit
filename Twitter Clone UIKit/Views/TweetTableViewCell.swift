49f4f04d5bc93c8b575be6f0d5af57350d0ebe52
EmerBV <emersmc@hotmail.com>
2022-10-02 20:45:41 +0200

    controllers added

---
+++ Twitter Clone UIKit/Views/TweetTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TweetTableViewCell.swift
+//  Twitter Clone UIKit
+//
+//  Created by Emerson Balahan Varona on 2/10/22.
+//
+
+import UIKit
+
+class TweetTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
