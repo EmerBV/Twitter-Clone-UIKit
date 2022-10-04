97f1183d0207c910af555a5d6960b00afd4c3086
EmerBV <emersmc@hotmail.com>
2022-10-04 21:22:13 +0200

    changing exact twitter icons and profile controller added

---
+++ Twitter Clone UIKit/Controllers/General/ProfileViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileViewController.swift
+//  Twitter Clone UIKit
+//
+//  Created by Emerson Balahan Varona on 4/10/22.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        navigationItem.title = "Profile"
+    }
+
+}
