5f0a1932655ea63c3de8252030b4e7c5209b0afd
EmerBV <emersmc@hotmail.com>
2022-10-20 23:12:34 +0200

    Setting up firebase auth, creating onboarding & register view controller

---
+++ Twitter Clone UIKit/Controllers/Onboarding/OnboardingViewController.swift
@@ -0,0 +1,110 @@
+//
+//  OnboardingViewController.swift
+//  Twitter Clone UIKit
+//
+//  Created by Emerson Balahan Varona on 20/10/22.
+//
+
+import UIKit
+
+class OnboardingViewController: UIViewController {
+    // Creamos vista de bienvenida
+    private let welcomeLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 0
+        label.text = "See what's happening in the world right now."
+        label.font = .systemFont(ofSize: 32, weight: .heavy)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = .center
+        label.textColor = .label
+        return label
+    }()
+    
+    // Creamos botón de crear la cuenta
+    private let createAccountButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Create account", for: .normal)
+        button.titleLabel?.font = .systemFont(ofSize: 24, weight: .bold)
+        button.backgroundColor = UIColor(red: 29/255, green: 161/255, blue: 242/255, alpha: 1)
+        button.layer.masksToBounds = true
+        button.tintColor = .white
+        button.layer.cornerRadius = 30
+        return button
+    }()
+    
+    private let promptLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.tintColor = .gray
+        label.text = "Have an account already?"
+        label.font = .systemFont(ofSize: 14, weight: .regular)
+        return label
+    }()
+    
+    private let loginButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Login", for: .normal)
+        button.titleLabel?.font = .systemFont(ofSize: 14)
+        button.tintColor = UIColor(red: 29/255, green: 161/255, blue: 242/255, alpha: 1)
+        return button
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        // Cargamos la pantalla de bienvenida
+        view.addSubview(welcomeLabel)
+        view.addSubview(createAccountButton)
+        view.addSubview(promptLabel)
+        view.addSubview(loginButton)
+        
+        // Esto sirve para que al presionar el botón de crear cuenta nos redirija a dicha página
+        createAccountButton.addTarget(self, action: #selector(didTapCreateAccount), for: .touchUpInside)
+        
+        // Esto es para crear los constraints
+        configureConstraints()
+        
+    }
+    
+    // Creamos la función de redirigir a la página de registro al presionar el botón de crear cuenta
+    @objc private func didTapCreateAccount() {
+        // Le pasamos la vista del registro
+        let vc = RegisterViewController()
+        navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    
+    private func configureConstraints() {
+        // Configuramos los constraints para ubicar donde queremos que aparezca la frase de bienvenida
+        let welcomeLabelConstraints = [
+            welcomeLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            welcomeLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            welcomeLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ]
+        
+        let createAccountButtonConstraints = [
+            createAccountButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            createAccountButton.topAnchor.constraint(equalTo: welcomeLabel.bottomAnchor, constant: 20),
+            createAccountButton.widthAnchor.constraint(equalTo: welcomeLabel.widthAnchor, constant: -20),
+            createAccountButton.heightAnchor.constraint(equalToConstant: 60)
+        ]
+        
+        let promptLabelConstraints = [
+            promptLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            promptLabel.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -60)
+        ]
+        
+        let loginButtonConstraints = [
+            loginButton.centerYAnchor.constraint(equalTo: promptLabel.centerYAnchor),
+            loginButton.leadingAnchor.constraint(equalTo: promptLabel.trailingAnchor, constant: 10)
+        ]
+        
+        // Activamos los constraints
+        NSLayoutConstraint.activate(welcomeLabelConstraints)
+        NSLayoutConstraint.activate(createAccountButtonConstraints)
+        NSLayoutConstraint.activate(promptLabelConstraints)
+        NSLayoutConstraint.activate(loginButtonConstraints)
+    }
+}
